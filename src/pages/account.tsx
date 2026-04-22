4e0234a187810418e491c25a06654f6379c4a99e
rynnnnc <ry3ncao@gmail.com>
2026-04-22 13:55:08 -0400

    login page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -4,23 +4,22 @@ import {
     signInWithEmailAndPassword,
     GoogleAuthProvider,
     signInWithPopup,
+    sendPasswordResetEmail
 } from "firebase/auth";
-import { useState } from "react";
-import type { FormEvent } from "react";
+import { useEffect, useState } from "react";
 import "./account.css";
+import { FcGoogle } from "react-icons/fc";
 
 export default function Account() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isLogin, setIsLogin] = useState(true);
     const [error, setError] = useState("");
-
     const provider = new GoogleAuthProvider();
+    const [cooldown , setCooldown] = useState(0);
 
-    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+    async function handleSubmit() {
         setError("");
-
         try {
             if (isLogin) {
                 await signInWithEmailAndPassword(auth, email, password);
@@ -42,6 +41,21 @@ export default function Account() {
         }
     }
 
+    async function handleForgetPassword(){
+        if (cooldown > 0) return;
+        if (!email) {
+            setError("Please Enter Your Email First");
+            return;
+        }
+        try{
+            await sendPasswordResetEmail(auth, email);
+            setError("Password reset email sent")
+            setCooldown(30);
+        } catch (err : any){
+            setError(err.message);
+        }
+    }
+
     return (
         <div className="account-page">
             <h1 className="account-title">{isLogin ? "Log In" : "Sign Up"}</h1>
@@ -57,7 +71,12 @@ export default function Account() {
                     />
                 </div>
                 <div className="input-group">
-                    <label>Password</label>
+                    <div className="input-row">
+                    <label>Password</label> 
+                    <span className = "forgot-password" onClick={handleForgetPassword}>
+                        Forgot Password?
+                    </span>
+                    </div>
                     <input
                         className="account-input"
                         type="password"
@@ -70,13 +89,15 @@ export default function Account() {
                 </button>
             </form>
 
-            <button className="google-button" onClick={handleGoogleSignIn}>
-                Continue with Google
-            </button>
-
-            <button className="account-toggle" onClick={() => setIsLogin(!isLogin)}>
-                {isLogin ? "Create an account" : "Already have an account?"}
-            </button>
+        
+            <div className="account-toggle">
+                {isLogin ? "Don't have an account?" : "Already have an account?"}
+                <span className = "account-link"
+                onClick={() => setIsLogin(!isLogin)}>
+                    {isLogin ? "Sign Up" : "Log In"}
+                </span>
+            </div>
+            <FcGoogle className = 'google-button' onClick={handleGoogleSignIn}/>
 
             {error && <p className="account-error">{error}</p>}
         </div>
